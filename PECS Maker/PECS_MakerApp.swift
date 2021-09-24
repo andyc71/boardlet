56a85764e7948dcb911e5a46a3e003fc0b35cbf6
andyc71 <andrewclynes@hotmail.com>
2021-09-24 14:03:07 +0100

    Initial Commit

---
+++ PECS Maker/PECS_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PECS_MakerApp.swift
+//  PECS Maker
+//
+//  Created by Andy on 24/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct PECS_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

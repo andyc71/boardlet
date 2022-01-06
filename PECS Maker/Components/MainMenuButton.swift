a1d73aafccda125f05c0f1369961f6655fe68e85
andyc71 <andrewclynes@hotmail.com>
2022-01-06 11:57:39 +0000

    Add spacers so that the button can grow it's height (for use in configuration where we want equal height buttons).

--- PECS Maker/Components/MainMenuButton.swift
+++ PECS Maker/Components/MainMenuButton.swift
@@ -25,14 +25,19 @@ struct MainMenuButton: View {
      
         Button(action: { action() }) {
             ConditionalStack(isHorizonalStack: isHorizontal) {
+                Spacer()
+                    .frame(minHeight: 0, idealHeight: 0)
                 if let systemIconName = systemIconName {
                     Image(systemName: systemIconName)
                         .font(buttonFontImage)
-                        .padding(2)
+                        .padding(0)
                 }
                 ZStack {
                     Text(text)
+                        .lineLimit(2)
                         .font(buttonFontTitle)
+                        .fixedSize(horizontal: false, vertical: true)
+                    
                     if showCheckMark {
                         Image(systemName: "checkmark.circle.fill")
                             .font(.largeTitle)
@@ -42,9 +47,13 @@ struct MainMenuButton: View {
                     }
                 }
                 .frame(maxWidth: .infinity)
+                .padding(EdgeInsets(top: -8, leading: 0, bottom: 0, trailing: 0))
+                Spacer()
+                            .frame(minHeight: 0, idealHeight: 0)
+
             }
             .frame(minWidth: 0, maxWidth: .infinity)
-            .padding()
+            .padding(4)
             .foregroundColor(.white)
             .background(Color(isSecondary ? ColorNames.green : ColorNames.brightBlue) )
             .cornerRadius(20)

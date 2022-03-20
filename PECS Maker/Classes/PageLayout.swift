434f784fee519e83a8d8a672f6b3fdbac49120aa
andyc71 <andrewclynes@hotmail.com>
2022-03-20 07:00:04 +0000

    Change order of page sizes

--- PECS Maker/Classes/PageLayout.swift
+++ PECS Maker/Classes/PageLayout.swift
@@ -11,7 +11,12 @@ struct PageLayoutType {
     static func forPageSize(_ pageSize: PageSize) -> [CGSize] {
         switch pageSize {
         case .a4, .usLetter:
-            return [CGSize(width: 2, height: 4), CGSize(width: 3, height: 4), CGSize(width: 2, height: 3), CGSize(width: 2, height: 2),  ]
+            return [
+                CGSize(width: 2, height: 2),
+                CGSize(width: 2, height: 3),
+                CGSize(width: 2, height: 4),
+                CGSize(width: 3, height: 4),
+            ]
         case .photo10by15:
             return [CGSize(width: 1, height: 1), CGSize(width: 1, height: 2) ]
         }

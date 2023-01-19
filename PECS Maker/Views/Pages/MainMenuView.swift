3c6034b36000be9ff5ffa2867c8f8c8574b64c8c
andyc71 <andrewclynes@hotmail.com>
2023-01-19 09:17:04 +0000

    Extract ZLPicker wrapper into a view extension

--- PECS Maker/Views/Pages/MainMenuView.swift
+++ PECS Maker/Views/Pages/MainMenuView.swift
@@ -379,7 +379,7 @@ struct MainMenuView: View, Equatable {
                 
                 MainMenuButton(action: {
                     //action = .selectPhoto
-                    selectPhotos()
+                    selectPhotos(photoBrowserData: pageLayoutState.photoBrowserData)
                 }, systemIconName: "photo", text: L10n.MainMenu.selectPhotosButton, showCheckMark: pageLayoutState.photoBrowserData.photoItems.count>0)
                 .selectionAndPadding(isSelected: action == .selectPhoto, isForSplitView: isForSplitView)
                 .accessibility(identifier: AccessibilityIdentifiers.MainMenu.selectPhotoButton)
@@ -524,103 +524,6 @@ struct MainMenuView: View, Equatable {
     }
     
     
-    func selectPhotos() {
-        let scene = UIApplication.shared.connectedScenes.first
-        let root = (scene as? UIWindowScene)?.windows.first?.rootViewController
-        if root != nil {
-            
-            ZLPhotoConfiguration.default().allowSelectImage = true
-            ZLPhotoConfiguration.default().allowSelectVideo = false
-            ZLPhotoConfiguration.default().allowEditImage = true
-            //ZLPhotoConfiguration.default().editImageTools = [.clip, .filter]
-            ZLPhotoConfiguration.default().allowTakePhoto = true
-            ZLPhotoConfiguration.default().maxSelectCount = AppSettings.maxSelectionsInPhotoPicker
-            ZLPhotoConfiguration.default().showSelectedPhotoPreview = false
-            ZLPhotoConfiguration.default().allowSelectOriginal = false
-            ZLPhotoConfiguration.default().saveNewImageAfterEdit = false
-            ZLPhotoConfiguration.default().showSelectedIndex = false
-            ZLPhotoConfiguration.default().allowPreviewPhotos = false
-            ZLPhotoConfiguration.default().showPreviewButtonInAlbum = false
-            //ZLPhotoConfiguration.default().editImageClipRatios = [ZLImageClipRatio(title: "", whRatio: CGFloat(1) / aspectRatio)]
-            //ZLPhotoConfiguration.default().frontFacingCamera = true
-            //ZLPhotoConfiguration.default().supportedCameraOrientations = [.all]
-            //ZLPhotoConfiguration.default().defaultCameraPosition = .front
-            
-            //Custom camera doesn't rotate properly on iPad, and I can't
-            //see a good reason for using it anyway.
-            ZLPhotoConfiguration.default().useCustomCamera = false
-            
-            
-            let theme = ZLPhotoUIConfiguration.default()
-            
-            //Bottom toolbar theme
-            let colorScheme = currentTheme.buttonStyle(for: .primary)
-            
-            //**Thunbnail page.
-            theme.thumbnailBgColor = currentTheme.backgroundColor
-            
-            //Nav bar theme
-            theme.navViewBlurEffectOfAlbumList = nil
-            theme.navTitleColor = currentTheme.headerStyle.textColor
-            theme.navBarColor = currentTheme.headerStyle.backgroundColor
-            theme.navCancelButtonStyle = .text
-            
-            //Bottom toolbar
-            theme.bottomViewBlurEffectOfAlbumList  = nil
-            theme.bottomToolViewBgColor = currentTheme.headerStyle.backgroundColor
-            
-            //Done button
-            theme.bottomToolViewBtnNormalTitleColor = colorScheme.textColor
-            theme.bottomToolViewBtnNormalBgColor = colorScheme.fillColor
-            
-            /*
-             //Preview button
-             theme.bottomToolViewBtnNormalTitleColor = currentTheme.headerStyle.backButtonTextColor
-             
-             // Preview page
-             theme.previewVCBgColor = currentTheme.backgroundColor
-             
-             //Preview page - nav bar
-             theme.navViewBlurEffectOfPreview = nil
-             theme.navTitleColorOfPreviewVC = currentTheme.headerStyle.backButtonTextColor
-             theme.navBarColorOfPreviewVC = currentTheme.headerStyle.backgroundColor
-             //theme.sheetBtnTitleColor = currentTheme.headerStyle.backButtonTextColor
-             
-             
-             //theme.bottomViewBlurEffectOfPreview = nil
-             */
-            
-            
-            //                theme.bottomToolViewBtnNormalTitleColor = colorScheme.textColor
-            //                theme.bottomToolViewBtnNormalBgColor = colorScheme.fillColor
-            //                theme.bottomToolViewDoneBtnNormalTitleColor = colorScheme.textColor
-            //                theme.bottomToolViewBtnNormalBgColorOfPreviewVC = colorScheme.fillColor
-            //                theme.bottomToolViewDoneBtnNormalTitleColorOfPreviewVC = colorScheme.textColor
-            //                theme.bottomToolViewBtnNormalTitleColorOfPreviewVC = colorScheme.textColor
-            //                theme.bottomToolViewDoneBtnNormalTitleColor = colorScheme.textColor
-            
-            
-            let ac = ZLPhotoPreviewSheet(selectedAssets: pageLayoutState.photoBrowserData.photoAssets)
-            
-            ac.selectImageBlock = { (images, assets, isOriginal) in
-                DispatchQueue.main.async {
-                    var photoItems = [PhotoItem]()
-                    for i in 0..<images.count {
-                        let image = images[i]
-                        let asset = assets[i]
-                        let photoItem = PhotoItem(image: image, asset: asset)
-                        photoItems.append(photoItem)
-                    }
-                    //Updating the photoBrowserData will automatically call save on the repo.
-                    pageLayoutState.photoBrowserData.photoItems = photoItems
-                    //self.save()
-                }
-            }
-            
-            ac.showPhotoLibrary(sender: root!)
-        }
-    }
-    
 }
 
 extension View {
@@ -643,6 +546,7 @@ extension View {
         }
     }
 }
+
 
 
 
